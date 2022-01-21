f2d1fd9beca5ec8d3e96eb2b08a74ffab6818f82
andurebel <andu.trica@gmail.com>
2022-01-21 18:02:04 +0200

    added userSlice and Tailwind

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="">
+      <div>
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Username" />
+        <button>Add user</button>
+      </div>
+      <div>
+        <h1>User List below</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
